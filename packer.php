d2c0bab4522f1961499756c722bfcf83e45bc8a7
chulo <emilio.astarita@gmail.com>
2013-10-11 13:27:23 -0300

    Adding subdirectories in the packer.

--- packer.php
+++ packer.php
@@ -2,6 +2,8 @@
 
 $dir = dirname(__FILE__);
 $ignoreFiles = array(
+                     '.',
+                     '..',
                      'app-details',
                      '.git',
                      'easy-share.svg',
@@ -10,9 +12,14 @@ $files = array();
 $dh  = opendir($dir);
 while (false !== ($filename = readdir($dh))) {
   if (!in_array($filename, $ignoreFiles, true)) {
-    $files[] = "'".$filename."'";
+    if (is_dir($filename)) {
+      $files[] = "'".$filename."'/*";
+    } else {
+      $files[] = "'".$filename."'";
+    }
   }
 }
 
 $zipCmd = 'zip chrome-package.zip ' . implode(" ", $files);
+echo $zipCmd . "\n";
 shell_exec($zipCmd);
